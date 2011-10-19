cdd0b7c78d22f4788e54e370ec3960959f19b396
nhydock <n_hydock@comcast.net>
2011-10-19 12:56:21 -0400

    Players are different from actors in that they have jobs

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -1,5 +1,15 @@
 package actors;
 
+import jobs.Job;
+
 public class Player extends Actor {
 
+	Job job;		//the player's job
+	
+	public Player(Job j)
+	{
+		super("");
+		System.out.println(getAlive());
+		job = j;
+	}
 }
