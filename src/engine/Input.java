35868d8e5f7b31947a8fc0b5e8fe9b0064957f55
nhydock <n_hydock@comcast.net>
2011-10-19 12:55:50 -0400

    Input wrapper class

---
+++ src/engine/Input.java
@@ -0,0 +1,14 @@
+package engine;
+
+import java.awt.event.KeyEvent;
+public class Input {
+
+	//predefined input keys
+	public final static int KEY_A  = KeyEvent.VK_X;
+	public final static int KEY_B  = KeyEvent.VK_Z;
+	public final static int KEY_UP = KeyEvent.VK_UP;
+	public final static int KEY_DN = KeyEvent.VK_DOWN;
+	public final static int KEY_LT = KeyEvent.VK_LEFT;
+	public final static int KEY_RT = KeyEvent.VK_RIGHT;
+	
+}
