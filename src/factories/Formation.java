e66b440f9b300762bea1b0f9e8943cc9e96585fd
nhydock <n_hydock@comcast.net>
2011-10-20 15:31:56 -0400

    Start working on formation code

--- src/factories/Formation.java
+++ src/factories/Formation.java
@@ -1,5 +1,31 @@
 package factories;
 
-public class Formation extends ActorFactory{
+import java.util.ArrayList;
 
+import jobs.Job;
+import actors.Actor;
+import actors.Enemy;
+import actors.Player;
+
+public class Formation extends ActorFactory {
+	
+	ArrayList<Actor> actors;	
+	
+	public Formation()
+	{
+		actors = new ArrayList<Enemy>();
+	}
+	
+	@Override
+	public void make(String a)
+	{
+		actors.add(new Enemy(a));
+		System.out.println(actors.toString());		
+	}
+	
+	public void make(String[] foes)
+	{
+		for (String f : foes)
+			make(f);
+	}
 }
