4465999d0de31aab4fce1de3d790f292cc73c085
nhydock <n_hydock@comcast.net>
2011-10-24 15:04:40 -0400

    Player should be getting most of its attributes from Job

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -10,15 +10,33 @@ public class Player extends Actor {
 
 	Job job;		//the player's job
 	
-	public Player(Job j)
+	public Player(String n, Job j)
 	{
-		super("");
 		System.out.println(getAlive());
 		job = j;
+		name = n.substring(0,4);	//char limit of 4
+		hp = j.getHP();
+		str = j.getStr();
+		def = j.getDef();
+		spd = j.getSpd();
+		evd = j.getEvd();
+		mag = j.getMag();
+		res = j.getRes();
+		loadSprites();
+	}
+	
+	/**
+	 * Retrieves the player's job (only the name)
+	 * @return
+	 */
+	public String getJob()
+	{
+		return job.getName();
 	}
 
 	@Override
 	protected void loadSprites() {
+		job.loadSprites();
 		sprites = job.getSprites();
 	}
 
