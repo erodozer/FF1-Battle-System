1a9a7357005c9a79d514ac732230257244a5e48b
nhydock <n_hydock@comcast.net>
2011-10-24 15:05:05 -0400

    GameScreen now renders things properly against a black background

--- src/engine/GameScreen.java
+++ src/engine/GameScreen.java
@@ -1,22 +1,47 @@
 package engine;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameScreen extends JFrame{
 
+
 	//frame resolution
-	final int FRAME_WIDTH = 256;
-	final int FRAME_HEIGHT = 240;
+	final int FRAME_WIDTH = 640;
+	final int FRAME_HEIGHT = 628;
 	
-	public GameScreen()
+	public class Background extends JPanel
 	{
-		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		this.setTitle("FF1 Battle System");
-		this.setVisible(true);
-		this.setBackground(Color.BLACK);
-		this.getGraphics().setClip(0, 0, this.WIDTH, this.HEIGHT);
+		@Override
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D) g;
+			this.setBackground(Color.black);
+			g2d.setPaint(Color.black);
+			g2d.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+		}
 	}
 
+	public GameScreen()
+	{
+		JComponent background = new Background();
+		background.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		
+		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		this.setTitle("FF1 Battle System");
+		
+		Container content = this.getContentPane();
+		content.setLayout(null);
+		this.setContentPane(background);
+		this.setVisible(true);	
+	}
+	
 }
