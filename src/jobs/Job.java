fa6d470f5ce34a298e842f27a57d99f83c043698
nhydock <n_hydock@comcast.net>
2011-10-24 15:04:10 -0400

    Some random work with Job to make it functioning at very least

--- src/jobs/Job.java
+++ src/jobs/Job.java
@@ -10,20 +10,58 @@ import engine.Sprite;
 
 public class Job {
 
-	private final String[] spriteNames = {"standing", "walk", "item", "cast", "victory"};
+	private final String[] spriteNames = {"stand", "walk", "item", "cast", "victory"};
 	private Sprite[] sprites;
 	
 	public void loadSprites()
 	{
 		sprites = new Sprite[spriteNames.length];
 		for (int i = 0; i < spriteNames.length; i++)
-			sprites[i] = new Sprite("data/actors/job/" + spriteNames[i] + ".png");
-
+			sprites[i] = new Sprite("data/actors/jobs/" + "Fighter" + "/"+ spriteNames[i] + ".png");
 	}
 	
 	public Sprite[] getSprites()
 	{
 		return sprites;
+	}
+
+	public String getName() {
+		return Class.class.getName();
+	}
+
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getStr() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getDef() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getSpd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getEvd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getRes() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 	
