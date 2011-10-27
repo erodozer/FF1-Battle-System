ff6594051d5405bfc9208fd424dee61c3fd61010
nhydock <n_hydock@comcast.net>
2011-10-26 20:16:36 -0400

    Merging and updating UML diagrams

---
+++ src/engine/TestGameScreen.java
@@ -0,0 +1,21 @@
+package engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import GUI.GameScreen;
+import actors.Enemy;
+
+public class TestGameScreen {
+
+	/**
+	 * Test drawing of the enemy sprite
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		GameScreen screen = new GameScreen();
+	}
+
+}
