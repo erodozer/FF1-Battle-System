e4f9eeeb8a00d8ab54becec680fea4a41eece89b
nhydock <n_hydock@comcast.net>
2011-11-02 14:33:38 -0400

    Changes to DamageBehavior design

--- src/DamageBehaviors/DamageBehavior.java
+++ src/DamageBehaviors/DamageBehavior.java
@@ -1,16 +1,18 @@
 package DamageBehaviors;
 
+import actors.Actor;
+
 public interface DamageBehavior {
 
 	/**
 	 * Calculates if the command will hit
 	 * @return
 	 */
-	public boolean didHit();
+	public boolean didHit(int bonus);
 	
 	/**
 	 * Calculates the command's damage output
 	 * @return
 	 */
-	public int calcDamage();
+	public int calcDamage(Actor attacker, Actor Target);
 }
