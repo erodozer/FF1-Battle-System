16395be1b8ecd30bb456a0c26e0c03e34401e67d
nhydock <n_hydock@comcast.net>
2011-11-02 14:30:17 -0400

    Started working on commands and damage behaviors

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,12 +1,29 @@
 package commands;
 
+import DamageBehaviors.DamageBehavior;
 import actors.Actor;
 
-public class Command {
+public abstract class Command {
 
-	public void execute(Actor target) {
-		// TODO Auto-generated method stub
+	protected String name;
+	protected Actor target;
+	protected Actor invoker;
+	protected int damage;
+	protected int speedBonus;
+	protected DamageBehavior db;
+	
+	/**
+	 * Executes the command
+	 * @param target
+	 */
+	abstract public void execute();
 		
+	/**
+	 * Retrieves the amount of damage dealt when the command was executed
+	 * @return
+	 */
+	public int getDamage()
+	{
+		return damage;
 	}
-
 }
