3fa1507909ace8619583a81344fb484e0267f75e
nhydock <n_hydock@comcast.net>
2011-11-05 14:19:43 -0400

    A bit of adjustments to the engine so it can be properly tested now

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,13 +1,20 @@
 package engine;
 
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFrame;
 
 import GUI.GameScreen;
 
 import groups.*;
 
-public class Engine implements KeyListener, Runnable {
+public class Engine extends Thread implements KeyListener {
 
 	private static Engine _instance;	//singleton instance
 	private Scene currentScene;			//current scene showing
@@ -16,7 +23,7 @@ public class Engine implements KeyListener, Runnable {
 	private Party party;				//main party
 	private Formation formation;		//formation encountered
 	
-	private GameScreen screen;
+	private static GameScreen screen;
 	
 	/**
 	 * Get the singleton instance
@@ -26,23 +33,33 @@ public class Engine implements KeyListener, Runnable {
 	{
 		if (_instance == null)
 			_instance = new Engine();
+		
 		return _instance;
 	}
 	
+	public static GameScreen getScreen()
+	{
+		return getInstance().screen;
+	}
+
 	public Engine()
 	{
-		
 		screen = new GameScreen();
 		//createScene("CreationScene");
+		screen.addKeyListener(this);
 		
 		_instance = this;
 	}
 	
-	private void createScene(String s)
+	public void createScene(String s)
 	{
 		try {
-			currentScene.stop();
-			currentScene = (Scene)Class.forName(s).newInstance();
+			if (currentScene != null)
+			{
+				currentScene.stop();
+				currentSceneThread.stop();
+			}
+			currentScene = (Scene)Class.forName("scenes."+s).newInstance();
 			currentSceneThread = new Thread(currentScene);
 			currentSceneThread.start();
 		} catch (InstantiationException e) {
@@ -53,30 +70,59 @@ public class Engine implements KeyListener, Runnable {
 			e.printStackTrace();
 		}
 	}
+
+	public Scene getCurrentScene()
+	{
+		return currentScene;
+	}
+	
+	public Party getParty()
+	{
+		return this.party;
+	}
+	
+	/**
+	 * Sets the party for the game
+	 * can only be set when the game starts
+	 */
+	public void setParty(Party p)
+	{
+		this.party = p;
+	}
 	
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		currentScene.keyPressed(arg0);
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void keyReleased(KeyEvent arg0) {}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void keyTyped(KeyEvent arg0) {}
 
 	@Override
-	public void run() {}
+	public void run() {
+		while (!isInterrupted())
+		{
+			screen.paint(screen.getGraphics());
+		}
+	}
 
 	// test client
     public static void main(String[] args) {
-    	new Thread(new Engine());
+    	Thread t = new Engine();
+    	
+    	Party p = new Party();
+    	p.add("TWIL", "RedMage");
+    	p.add("APPL", "Fighter");
+    	p.add("RNBW", "BlackBelt");
+    	p.add("RRTY", "BlackMage");
+    	
+    	Engine.getInstance().setParty(p);
+    	
+    	t.start();
+    	
+    	Engine.getInstance().createScene("BattleSystem");
     }
 }
