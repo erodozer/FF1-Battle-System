02b0f96bb2b7f05a7c441d8c94a340537870797e
nhydock <n_hydock@comcast.net>
2011-11-14 14:54:15 -0500

    Make spd bonus setting default start and reset actions for Command

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -14,8 +14,12 @@ public abstract class Command {
 	
 	/**
 	 * Adds effects to the actor upon assigning the command
+	 * Default is just adding the speed bonus
 	 */
-	abstract public void start();
+	public void start()
+	{
+		invoker.setSpd(invoker.getSpd()+speedBonus);		
+	}
 	
 	/**
 	 * Executes the command
@@ -27,8 +31,12 @@ public abstract class Command {
 	 * In the event of the command having a function/condition that
 	 * is supposed to last the duration of the turn, reset is implemented
 	 * to enforce that the command revert its effects when the turn is done
+	 * Default is just removing the speed bonus
 	 */
-	abstract public void reset();
+	public void reset()
+	{
+		invoker.setSpd(invoker.getSpd()-speedBonus);
+	}
 		
 	/**
 	 * Retrieves the amount of damage dealt when the command was executed
