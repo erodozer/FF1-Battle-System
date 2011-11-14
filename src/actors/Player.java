c7bf086ef7d0f23dae1b009a452a1e9ea35a8f4c
nhydock <n_hydock@comcast.net>
2011-11-14 14:52:04 -0500

    Players keep track of their sprites' coordinates (helpful for animation)

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -7,6 +7,8 @@ package actors;
  * Description: User controllable actors
  */
 
+import java.awt.Graphics;
+
 import engine.Sprite;
 
 import jobs.Job;
@@ -14,6 +16,19 @@ import jobs.Job;
 public class Player extends Actor {
 
 	Job job;		//the player's job
+	int state;		//the player's current state for animation
+					// 0 = stand, 1 = walk, 2 = act, 3 = cast, 4 = victory
+	Sprite drawSprite;
+	
+	//constant values for different state animations
+	public static final int STAND = 0;
+	public static final int WALK = 1;
+	public static final int ACT = 2;
+	public static final int CAST = 3;
+	public static final int VICT = 4;
+	
+	private double x;
+	private double y;
 	
 	public Player(String n, Job j)
 	{
@@ -38,11 +53,17 @@ public class Player extends Actor {
 	protected void loadSprites() {
 		job.loadSprites();
 		sprites = job.getSprites();
+		drawSprite = sprites[0];
 	}
 
+	/**
+	 * Returns the sprite to be rendered
+	 * Animation is very simple in FF1, only flipping between the current
+	 * animation frame for the state and the standing frame.
+	 */
 	@Override
 	public Sprite getSprite() {
-		return sprites[0];
+		return drawSprite;
 	}
 
 	/**
@@ -79,5 +100,97 @@ public class Player extends Actor {
 	{
 		return (int)(4.6666666669919*Math.pow(l, 3)+-13.99999999985*Math.pow(l,2)+
 			   37.3333333321*l+-27.9999999985);
+	}
+
+	/**
+	 * Sets the player's animation state with a string
+	 * @param string
+	 */
+	public void setState(int i) {
+		state = Math.max(STAND, Math.min(VICT, i));
+	}
+
+	/**
+	 * Returns the player's animation state
+	 * @return
+	 */
+	public int getState() {
+		return state;
+	}
+	
+	/**
+	 * Moves all the player's sprites to position
+	 * @param x
+	 * @param y
+	 */
+	public void setPosition(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+		for (Sprite s : sprites)
+		{
+			s.setX(this.x);
+			s.setY(this.y);
+		}
+	}
+
+	/**
+	 * Moves all the player's sprites x coord to position
+	 * @param x
+	 */
+	public void setX(double x)
+	{
+		this.x = x;
+		for (Sprite s : sprites)
+		{
+			s.setX(this.x);
+		}
+	}
+	
+	/**
+	 * Moves all the player's sprites y coord to position
+	 * @param y
+	 */
+	public void setY(double y)
+	{
+		this.y = y;
+		for (Sprite s : sprites)
+		{
+			s.setY(this.y);
+		}
+	}
+	
+	public double getX()
+	{
+		return x;
+	}
+	
+	public double getY()
+	{
+		return y;
+	}
+	
+	/**
+	 * Draws the actor to the screen and animates the graphic
+	 * @param g
+	 */
+	public void draw(Graphics g)
+	{
+		if (state == WALK && drawSprite == sprites[0])
+			drawSprite = sprites[1];
+		else if (state == ACT && drawSprite == sprites[0])
+			drawSprite = sprites[2];
+		else if (state == CAST && drawSprite == sprites[0])
+			drawSprite = sprites[3];
+		else if (state == VICT && drawSprite == sprites[0])
+			drawSprite = sprites[4];	
+		else
+			drawSprite = sprites[0];
+		drawSprite.paint(g);
+	}
+
+	public Sprite[] getSprites() {
+		// TODO Auto-generated method stub
+		return sprites;
 	}
 }
