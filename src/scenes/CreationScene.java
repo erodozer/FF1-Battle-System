b95d5f2ca52adae6035d6ddd99ad73f5f743ddc2
nhydock <n_hydock@comcast.net>
2011-11-18 14:24:39 -0500

    Add a holder class for the CreationScene

---
+++ src/scenes/CreationScene.java
@@ -0,0 +1,40 @@
+package scenes;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class CreationScene implements Scene {
+
+	
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
