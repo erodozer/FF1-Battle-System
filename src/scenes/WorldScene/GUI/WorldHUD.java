07acc30caeab5a7b62cea801269f3d94669ce21e
nhydock <n_hydock@comcast.net>
2011-12-20 17:09:21 -0500

    Leader sprite should be centered on its base

--- src/scenes/WorldScene/GUI/WorldHUD.java
+++ src/scenes/WorldScene/GUI/WorldHUD.java
@@ -19,7 +19,7 @@ public class WorldHUD extends HUD
 {
 	Engine e;
 	Sprite map;
-	Player leader;		//party leader, his sprite gets drawn
+	Sprite leader;		//party leader, his sprite gets drawn
 	
 	public WorldHUD(WorldSystem s)
 	{
@@ -30,7 +30,7 @@ public class WorldHUD extends HUD
 	
 	public void setLeader(Player p)
 	{
-		leader = p;
+		leader = p.getSprite();
 	}
 	
 	public void paint(Graphics g)
@@ -40,8 +40,8 @@ public class WorldHUD extends HUD
 		
 		g.translate(-((WorldSystem)parent).getX()*16 + (int)map.getWidth()/4, -((WorldSystem)parent).getY()*16  + (int)map.getHeight()/4);
 		map.paint(g);
-		leader.setX(((WorldSystem)parent).getX()*16);
-		leader.setY(((WorldSystem)parent).getY()*16);;
-		leader.draw(g);
+		leader.setX(((WorldSystem)parent).getX()*16-leader.getHeight()+16);
+		leader.setY(((WorldSystem)parent).getY()*16-leader.getWidth()/2+8);
+		leader.paint(g);
 	}
 }
