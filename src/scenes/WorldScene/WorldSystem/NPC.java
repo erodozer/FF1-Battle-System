f68e94589dad132172027f9b70ed7d66ba749a06
nhydock <n_hydock@comcast.net>
2011-12-20 22:30:53 -0500

    Work a little more on moving NPCs

--- src/scenes/WorldScene/WorldSystem/NPC.java
+++ src/scenes/WorldScene/WorldSystem/NPC.java
@@ -30,14 +30,8 @@ public class NPC {
 						//   between 0 and 10
 						//   -1 to not move at all
 	
-	//direction the sprite is facing
-	public static final int SOUTH = 1;
-	public static final int WEST = 2;
-	public static final int NORTH = 3;
-	public static final int EAST = 4;
-	
 	int moving = 0;
-	int direction = SOUTH;
+	int direction = WorldSystem.SOUTH;
 	
 	/**
 	 * Creates the npc
@@ -53,7 +47,9 @@ public class NPC {
 		}
 		else
 			name = node.get("name", "Jim");
-		
+		String pos = node.name().substring(node.name().indexOf('@'));
+		x = Integer.parseInt(pos.substring(0, pos.indexOf(',')));
+		y = Integer.parseInt(pos.substring(pos.indexOf(',')));
 	}
 	
 	public void interact()
@@ -68,39 +64,32 @@ public class NPC {
 	{
 		if (Math.random() > (1-(1.0/(speed+1))))
 		{
-			int x = this.x;
-			int y = this.y;
+			int[] pos = {this.x, this.y};
 			int dir = 0;
 			int counter = 0;
 			//keep checking until it can move for up to 5 times
 			while (counter <= 5)
 			{
 				dir = (int)(Math.random()*4) + 1;		//go random direction
-				if (dir == SOUTH)
-					y--;
-				else if (dir == NORTH)
-					y++;
-				else if (dir == EAST)
-					x++;
-				else if (dir == WEST)
-					x--;
 				
-				if (map.getPassibility(x, y))
+				pos = map.getCoordAhead(pos[0], pos[1], dir);
+				
+				if (map.getPassibility(pos[0], pos[1]))
 				{
 					walk();
+					direction = dir;
 					break;
 				}
 				else
 				{
-					x = this.x;
-					y = this.y;
+					pos = new int[]{this.x, this.y};
 					counter++;
 				}
 			}
-			xSlide = (x-this.x)/5.0;
-			ySlide = (y-this.y)/5.0;
-			this.x = x;
-			this.y = y;
+			xSlide = (pos[0]-x)/5.0;
+			ySlide = (pos[1]-y)/5.0;
+			x = pos[0];
+			y = pos[1];
 		}
 	}
 	
