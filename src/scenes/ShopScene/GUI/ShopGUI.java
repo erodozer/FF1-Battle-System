8748107b74a55062d8ed86ee1154eb934d1fefec
nhydock <n_hydock@comcast.net>
2012-01-12 22:24:41 -0500

    Add a little bit more to the shop's gui (still no where near complete)

--- src/scenes/ShopScene/GUI/ShopGUI.java
+++ src/scenes/ShopScene/GUI/ShopGUI.java
@@ -1,5 +1,7 @@
 package scenes.ShopScene.GUI;
 
+import item.Item;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -7,6 +9,7 @@ import java.awt.Graphics;
 import actors.Player;
 
 import scenes.HUD;
+import scenes.ShopScene.System.GreetState;
 import scenes.ShopScene.System.Shop;
 import scenes.ShopScene.System.ShopSystem;
 
@@ -20,20 +23,27 @@ import groups.Party;
 
 public class ShopGUI extends HUD {
 
+	//windows
 	Window nameWindow;
 	Window greetWindow;
 	Window greetSelect;
 	Window itemSelect;
 	Window moneyWindow;
 	
-	Sprite arrow;
-	Sprite shopKeeper;
+	Sprite arrow;			//cursor sprite
+	Sprite shopKeeper;		//shopkeeper sprite
 	
-	ShopSystem parent;
-	Party party;
+	ShopSystem parent;		//parent system
+	Party party;			//player's party
 			
-	Shop shop;
-	String[] greeting;
+	Shop shop;				//shop data
+	String[] greeting;		//shop greeting parsed with line wrap
+	Item[] items;			//items for sale
+	
+	int index;				//selection index
+	
+	String[] greetCommands = {"Buy", "Sell", "Exit"};
+							//entrance menu
 	
 	public ShopGUI(ShopSystem s)
 	{
@@ -53,6 +63,7 @@ public class ShopGUI extends HUD {
 		shopKeeper = shop.getShopKeeper();
 		shopKeeper.setX(ContentPanel.INTERNAL_RES_W/2-shopKeeper.getWidth());
 		shopKeeper.setY(80);
+		items = shop.getItems();
 		
 		int x;
 		int y;
@@ -76,7 +87,7 @@ public class ShopGUI extends HUD {
 	
 	@Override
 	public void update() {
-
+		index = parent.getState().getIndex();
 	}
 
 	/**
@@ -96,8 +107,32 @@ public class ShopGUI extends HUD {
 		for (int i = 0; i < greeting.length && i < 5; i++)
 			g.drawString(greeting[i], greetWindow.getX()+5, greetWindow.getY()+20+16*i);
 		
+		greetSelect.paint(g);
+		for (int i = 0; i < greetCommands.length; i++)
+			g.drawString(greetCommands[i], greetSelect.getX()+5, greetSelect.getY()+10+(18*i));
+		
 		moneyWindow.paint(g);
 		String s = party.getGold() + " G";
 		g.drawString(s, moneyWindow.getX() + moneyWindow.getWidth()-10 - GameScreen.fontMetrics.stringWidth(s), moneyWindow.getY()+20);
+		
+		if (!(parent.getState() instanceof GreetState))
+		{
+			itemSelect.paint(g);
+			for (int i = 0; i < items.length; i++)
+			{
+				g.drawString(items[i].getName(), itemSelect.getX()+10, itemSelect.getY()+10+(i*32));
+				String price = items[i].getPrice() + " G";
+				g.drawString(price, itemSelect.getX()+itemSelect.getWidth()-10-GameScreen.fontMetrics.stringWidth(price), itemSelect.getY()+22+(i*32));
+			}
+			arrow.setX(itemSelect.getX()-5);
+			arrow.setY(itemSelect.getY()+5+(index*32));
+		}
+		else
+		{
+			arrow.setX(greetSelect.getX()-5);
+			arrow.setY(greetSelect.getY()+10+(index*18));	
+		}
+		arrow.paint(g);
+		
 	}
 }
