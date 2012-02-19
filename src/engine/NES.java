b955b0fd1511bae000c8a686dd37f53b89bdd395
nhydock <n_hydock@comcast.net>
2012-02-18 19:29:31 -0500

    Creating a collection of NES palette colors to use

---
+++ src/engine/NES.java
@@ -0,0 +1,19 @@
+package engine;
+
+/**
+ * NES
+ * @author nhydock
+ *
+ *	Collection of colors that are used 
+ *	in the NES version of FF1
+ */
+import java.awt.Color;
+public class NES
+{
+	public static final Color BLUE = Color.decode("#24188c");
+	public static final Color GREEN = Color.decode("#005000");
+	public static final Color TEAL = Color.decode("#3cbcfc");
+	public static final Color VIOLET = Color.decode("#8c0074");
+	public static final Color BLACK = Color.decode("#000000");
+	
+}
