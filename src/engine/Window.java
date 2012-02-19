5d71faf237fde0ed4529137b512b0a69774acf89
nhydock <n_hydock@comcast.net>
2012-02-18 19:27:59 -0500

    Simplify window rendering

--- src/engine/Window.java
+++ src/engine/Window.java
@@ -3,11 +3,13 @@ package engine;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 /**
  * Window
@@ -20,15 +22,9 @@ public class Window {
 	Color bg;					//background color of the window
 	
 	//image parts
-	BufferedImage tlcorner;		//top-left corner
-	BufferedImage blcorner;		//bottom-left corner
-	BufferedImage trcorner;		//top-right corner
-	BufferedImage brcorner;		//bottom-right corner
+	Sprite image;
 	
-	BufferedImage left;			//left side
-	BufferedImage right;		//right side
-	BufferedImage top;			//top side
-	BufferedImage bottom;		//bottom side
+	Image window;
 	
 	//dimensions
 	int width  = 1;
@@ -47,7 +43,7 @@ public class Window {
 	 */
 	public Window(int a, int b, int w, int h)
 	{
-		this(a, b, w, h, Color.black);
+		this(a, b, w, h, NES.BLACK);
 	}
 	
 	/**
@@ -60,24 +56,39 @@ public class Window {
 	 */
 	public Window(int a, int b, int w, int h, Color c)
 	{
-		try {
-			tlcorner = ImageIO.read(new File("data/hud/tlcorner.png"));
-			blcorner = ImageIO.read(new File("data/hud/blcorner.png"));
-			trcorner = ImageIO.read(new File("data/hud/trcorner.png"));
-			brcorner = ImageIO.read(new File("data/hud/brcorner.png"));
-			left = ImageIO.read(new File("data/hud/left.png"));
-			right = ImageIO.read(new File("data/hud/right.png"));
-			top = ImageIO.read(new File("data/hud/top.png"));
-			bottom = ImageIO.read(new File("data/hud/bottom.png"));
-		} 
-    	catch (IOException e) {
-    		System.err.println("Files not available to draw windows");
-    	}
+		window = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		image = new Sprite("hud/window.png", 3, 3);
+
+		Graphics g = window.getGraphics();
+
+		int oW = (int) image.getWidth();
+		int oH = (int) image.getHeight();
+
+		int[][] winStruct = {{0,0},{oW,oH},{w-oW,h-oH}};
+		int[][] dimensions = {{oW,oH}, {w-(oW*2), h-(oH*2)}, {oW, oH}};
+		
+		
 		x = a;
 		y = b;
 		width = w;
 		height = h;
 		bg = c;
+
+		g.setColor(bg);
+		g.fillRect(0, 0, w, h);
+
+		
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++)
+			{
+				image.setFrame(x+1, y+1);
+				image.setX(winStruct[x][0]);
+				image.setY(winStruct[y][1]);
+				image.scale(dimensions[x][0], dimensions[y][1]);
+				image.paint(g);
+			}
+
+	
 	}
 
 	/**
@@ -126,28 +137,6 @@ public class Window {
 	 */
 	public void paint(Graphics g)
 	{
-		Graphics2D g2 = (Graphics2D) g;
-		Color currentColor = g2.getColor();		//color before drawing the window
-		g2.setColor(Color.black);
-		g2.fillRect(x, y, width, height);
-		g2.setColor(bg);
-		g2.fillRect(x+tlcorner.getWidth()/2-2, y + tlcorner.getHeight()/2-2,
-					width - (tlcorner.getWidth()/2 + trcorner.getWidth()/2) + 4,
-					height - (tlcorner.getHeight()/2 + trcorner.getHeight()/2) + 4
-					);
-		g2.drawImage(tlcorner, null, x, y);
-		g2.drawImage(blcorner, null, x, y+height-blcorner.getHeight());
-		g2.drawImage(trcorner, null, x+width - trcorner.getWidth(), y);
-		g2.drawImage(brcorner, null, x+width - brcorner.getWidth(), y+height-brcorner.getHeight());
-			
-		g2.drawImage(top, x+tlcorner.getWidth(), y, 
-					 width - (tlcorner.getWidth() + trcorner.getWidth()), top.getHeight(), null);
-		g2.drawImage(bottom, x+blcorner.getWidth(), y+height-blcorner.getHeight(), 
-				 width - (blcorner.getWidth() + brcorner.getWidth()), bottom.getHeight(), null);
-		g2.drawImage(right, x+width - trcorner.getWidth(), y+trcorner.getHeight(), 
-				     right.getWidth(), height - (trcorner.getHeight() + brcorner.getHeight()), null);
-		g2.drawImage(left, x, y+tlcorner.getHeight(), 
-				     left.getWidth(), height - (tlcorner.getHeight() + blcorner.getHeight()), null);		
-		g2.setColor(currentColor);
+		g.drawImage(window, x, y, null);
 	}
 }
