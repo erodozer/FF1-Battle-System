5ad0b3e69e3fd8fde6ed07fa9e7659d8c03d6fb8
nhydock <n_hydock@comcast.net>
2012-03-29 12:53:30 -0400

    Work on Victory state a bit more to get distributing gold and exp and level up working

--- src/scenes/BattleScene/GUI/VictoryDisplay.java
+++ src/scenes/BattleScene/GUI/VictoryDisplay.java
@@ -2,6 +2,7 @@ package scenes.BattleScene.GUI;
 
 import java.awt.Graphics;
 
+import engine.SFont;
 import engine.Sprite;
 import engine.Window;
 import scenes.BattleScene.System.*;
@@ -18,11 +19,19 @@ public class VictoryDisplay extends Sprite{
 	Sprite arrow;
 	BattleSystem parent;	
 	
+	SFont f = SFont.loadFont("default");	
+	//font
+	
 	public VictoryDisplay(int x, int y)
 	{
 		super(null);
-		windows = new Window[]{new Window(x, y+48, 180, 34), new Window(x, y, 90, 34), new Window(x+82, y, 90, 34), 
-							   new Window(x, y+24, 90, 34), new Window(x+82, y+24, 90, 34)};
+		windows = new Window[]{new Window(x, y+48, 180, 34), 		//displays message
+							   new Window(x, y, 90, 34), 			//displays "experience" or person's name
+							   new Window(x+82, y, 90, 34), 		//displays exp gained	or "leveled up!"
+							   new Window(x, y+24, 90, 34), 		//displays "g"
+							   new Window(x+82, y+24, 90, 34),		//displays g gained
+							   new Window(x, y+24, 180, 34)			//displays stat gained
+							};		
 	}
 
 
@@ -37,13 +46,15 @@ public class VictoryDisplay extends Sprite{
 	@Override
 	public void paint(Graphics g)
 	{
-		
-		if (((VictoryState)parent.getState()).getStep() == 0)
+		VictoryState state = ((VictoryState)parent.getState());
+		//generic victory message
+		if (state.getStep() == 0)
 		{
 			windows[0].paint(g);
 			g.drawString("All enemies annihilated", windows[0].getX() + 10, windows[0].getY() + 20);
 		}
-		else
+		//display get experience and gold
+		else if (state.getStep() == 1)
 		{
 			windows[1].paint(g);
 			g.drawString("Experience", windows[1].getX() + 10, windows[1].getY() + 20);
@@ -55,5 +66,16 @@ public class VictoryDisplay extends Sprite{
 			windows[4].paint(g);
 			g.drawString(parent.getFormation().getGold()+"G", windows[4].getX() + 10, windows[4].getY() + 20);
 		}
+		//display character leveling up
+		else if (state.getStep() == 3)
+		{
+			windows[1].paint(g);
+			f.drawString(g, state.getPlayer().getName(), 0, 10, windows[1]);
+			windows[2].paint(g);
+			f.drawString(g, "Leveled Up!", 0, 10, windows[2]);
+			windows[3].paint(g);
+			f.drawString(g, state.getMessage(), 0, 10, windows[3]);
+		}
+		
 	}
 }
--- src/scenes/BattleScene/System/VictoryState.java
+++ src/scenes/BattleScene/System/VictoryState.java
@@ -2,10 +2,15 @@ package scenes.BattleScene.System;
 
 import java.awt.event.KeyEvent;
 
+import actors.Player;
+
 import scenes.GameState;
 
+import engine.Engine;
 import engine.Input;
 import engine.MP3;
+import groups.Formation;
+import groups.Party;
 
 /**
  * VictoryState
@@ -19,13 +24,31 @@ public class VictoryState extends GameState {
 
 	int step = 0;		//step 0 = show "all enemies eliminated"
 						//step 1 = show exp and g gained
+						//step 2 = show level up messages
 
+	int exp = 0;
+	int g = 0;
+	
+	Formation f;
+	Party p;
+	
+	int leveledUp = 0;
+	
+	int step2 = 0;		//for level up, display player
+	int step3 = 0;		//for counter on displaying message for level up
+	
+	String[] levMessage = new String[0];
+						//message for level up
+	String message;
+	
 	/**
 	 * Constructs state
 	 * @param p
 	 */
 	VictoryState(BattleSystem p) {
 		super(p);
+		f = p.getFormation();
+		this.p = Engine.getInstance().getParty();
 	}
 	
 	/**
@@ -36,13 +59,80 @@ public class VictoryState extends GameState {
 		MP3.stop();
 		new MP3("data/audio/victory.mp3").play();
 		step = 0;
+		
+		//distribute the exp and g
+		exp = f.getExp()/p.getAlive();
+		g = f.getGold();
+		
+		for (Player player: p)
+		{
+			player.setExp(player.getExp()+exp);
+			if (player.getExpToLevel() <= 0)
+				leveledUp++;
+		}
+		p.addGold(g);
+	
+		
 	}
 
 	/**
 	 * Do nothing
 	 */
 	@Override
-	public void handle() {}
+	public void handle() {
+		try {
+			Thread.sleep(500);
+			
+			//check to see if the level up messages should be shown or not
+			if (step == 1)
+			{
+				if (leveledUp > 0)
+				{
+					step = 2;
+				}
+				else
+					step = 4;
+			}
+			
+			//advances through players to level them up and display level up messages
+			if (step == 2)
+			{
+				Player player = p.get(step2);
+				player.levelUp();
+				step = 3;
+				step3 = 0;
+				levMessage = player.previewLevelUp().split("\n");
+				message = levMessage[step3];
+			}
+			//display each stat being leveled up
+			else if (step == 3)
+			{
+				if (step3 >= levMessage.length)
+				{
+					step = 2;
+					step3 = 0;
+					step2++;
+				}
+				else
+				{
+					step3++;
+					message = levMessage[step3];
+				}
+			}
+			//end victory scene
+			else if (step >= 4)
+			{
+				finish();
+			}
+			else
+			{
+				step++;
+			}
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+	}
 
 	/**
 	 * Go back to the map
@@ -74,5 +164,15 @@ public class VictoryState extends GameState {
 	public int getStep()
 	{
 		return step;
+	}
+	
+	public String getMessage()
+	{
+		return message;
+	}
+	
+	public Player getPlayer()
+	{
+		return p.get(step2);
 	}
 }
