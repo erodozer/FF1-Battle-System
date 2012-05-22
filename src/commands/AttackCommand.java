c5db58bbc4c7e8ba3a7feca5f89958f22e08e8af
nhydock <n_hydock@comcast.net>
2012-05-22 00:48:10 -0400

    Renamed two commands

---
+++ src/commands/AttackCommand.java
@@ -0,0 +1,129 @@
+package commands;
+
+import graphics.Animation;
+import actors.Actor;
+import actors.Player;
+
+public class AttackCommand extends Command {
+
+	final int base = 168;	//this is the base hit chance value
+	
+	/**
+	 * Constructs a basic physical attack command
+	 * @param a
+	 * @param t
+	 */
+	public AttackCommand(Actor a, Actor[] t)
+	{
+		super(a, t);
+		anim = new Animation("attack");
+		name = "Attack";
+		speedBonus = 25;
+	}
+	
+	/**
+	 * Executes the attack
+	 */
+	@Override
+	public void execute() {
+		if (currentTargetIndex >= targets.length)
+			return;
+		
+		currentTarget = targets[currentTargetIndex];
+		
+		//make sure the animation is in relation to the target sprite
+		if (anim.getRelationType() == 2)
+			anim.setRelation(invoker.getSprite());
+		else if (anim.getRelationType() == 1)
+			anim.setRelation(currentTarget.getSprite());
+		else
+			anim.setRelation(null);
+	
+	    //reset damage
+		damage = 0;
+		hits = (1+(invoker.getAcc()/32))*1;
+		if (!currentTarget.getAlive()){
+			currentTargetIndex++;
+			execute();
+		}
+		
+		/*
+		 * Calculate the chance of hitting
+		 * depending on the skill of the attacker, multiple hits
+		 * may land and do damage to the target
+		 */
+		int H = invoker.getAcc();
+		int E = currentTarget.getEvd();
+		int chance = base + H - E;
+		
+		int hit = (int)(Math.random()*200);
+		if (hit < chance)
+		{
+			/*
+			 	Weapons not yet implemented
+			 	if (invoker instanceof Player)
+					db.isCritical((hit < ((Player)invoker).getWeapon().getCrit()));	
+			 	else
+			 		db.isCritical(dunno yet);
+			 */
+			int amount = calculateDamage(false);
+			for (int i = 0; i < hits; i++)
+				damage += amount;
+		}
+		else
+			hits = 0;
+		currentTarget.setHP(currentTarget.getHP()-damage);
+		
+		//change display tag
+		name = "";
+		if (hits > 1)
+			name = hits + " hits";
+		
+		currentTargetIndex++;
+	}
+	
+	
+	/**
+	 * Returns the name of the command
+	 */
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+
+	/**
+	 * Reset the name after the turn has been executed so it displays
+	 * properly in the menu
+	 */
+	@Override
+	public void reset()
+	{
+		super.reset();
+		name = "Attack";
+	}
+	
+	/**
+	 * Calculates the damage an attack will do
+	 */
+	@Override
+	protected int calculateDamage(boolean critical) {
+		int D;
+
+        D = (int)((Math.random()+1)*(invoker.getStr()/2))+1;
+        
+        //Black belts are special that their attack is their level*2 when unarmed
+		if (invoker instanceof Player)
+		    if (((Player)invoker).getJobName().equals("Bl. BELT"))
+		        D = invoker.getLevel()*2;
+		
+		int A = currentTarget.getDef();
+		return Math.max(1, D * (critical?2:1) - A);
+	}
+
+	@Override
+	public boolean isDone() {
+		return currentTargetIndex >= targets.length;
+	}
+
+}
---
+++ src/commands/FleeCommand.java
@@ -0,0 +1,55 @@
+package commands;
+
+import scenes.BattleScene.BattleScene;
+import scenes.BattleScene.System.*;
+import engine.Engine;
+import actors.Actor;
+
+/**
+ * Flee.java
+ * @author nhydock
+ *
+ *	Flee command tries to end the battle early
+ */
+
+public class FleeCommand extends Command {
+    
+	public FleeCommand(Actor a, Actor[] t)
+	{
+		super(a, t);
+	    invoker = a;
+		name = "Run";
+	}
+	
+	/**
+	 * Executes the running
+	 * if run is successful, it makes use of the hits variable and sets it to
+	 * 1, if it's unsuccessful, it sets it to 0
+	 */
+	@Override
+	public void execute() {
+	    if (invoker.getLuck() > Math.random()*(invoker.getLevel()*15))
+	        hits = 1;
+	    else
+	        hits = 0;
+    }
+
+	/**
+	 * Use calculate damage to calculate the chance of escaping from the targets
+	 */
+	@Override
+	protected int calculateDamage(boolean critical) {
+		return 0;
+	}
+
+	@Override
+	public void reset()
+	{
+		name = "Run";
+	}
+
+	@Override
+	public boolean isDone() {
+		return true;
+	}
+}
