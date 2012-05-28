cd9c7cca20f1585c85c6d4654404d0934b78a087
nhydock <n_hydock@comcast.net>
2012-05-28 01:09:17 -0400

    Try cleaning up the transition effect by using LookupOp.

--- src/engine/ContentPanel.java
+++ src/engine/ContentPanel.java
@@ -1,10 +1,16 @@
 package engine;
 
+import graphics.SFont;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.ByteLookupTable;
 import java.awt.image.IndexColorModel;
+import java.awt.image.LookupOp;
+import java.awt.image.LookupTable;
+import java.awt.image.ShortLookupTable;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,36 +18,57 @@ import java.util.Arrays;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+/**
+ * ContentPanel
+ * @author nhydock
+ *
+ *	Content Panel holds all the rendering code for drawing the 
+ *	graphics into a graphics object.  It also handles stretching
+ *	the game's resolution into the window's resolution and fade
+ *	transitioning.
+ */
 public class ContentPanel extends JPanel{
 
+	
+	//default clear color for the buffer
 	private static final Color DEFAULT_CLEAR_COLOR = Color.BLACK;
 
-	//256 because of bit limit for raster
-	private static final int TRANSITIONLIMIT = 256;	
+	//255 because of bit limit for raster
+	private static final int TRANSITIONLIMIT = 255;	
 
 	//NES Native resolution
 	public static final int INTERNAL_RES_W = 256;
 	public static final int INTERNAL_RES_H = 224;
 	
+	
 	private Image dbImage;				//image to draw to
 	private Graphics dbg;				//graphics context of the component
-	private Engine engine;
+	private Image tBuffer;				//transition saved buffer
+	private Graphics tGraphics;			//transition saved graphics component
+	private Engine engine;				//engine instance for getting current scene for drawing
 	
 	private Color clearColor;			//color the background clears to
 	
+	
+	//TRANSITION VARIABLES
 	int transition = TRANSITIONLIMIT;	//transition timer
-	private static int TRANSITIONRATE = TRANSITIONLIMIT/GameScreen.FRAMES_PER_SECOND;
-										//rate at which transitions occur
+	private static int TRANSITIONRATE = 1;
+									//rate at which transitions occur
+	
 	private BufferedImage transFader;	//the transition fader grayscale image
-	private BufferedImage transBuffer;
+	private BufferedImage transBuffer;	//the current transition fader buffer after applying a LookupOp
 	
 	private boolean transIn = true;	//type of transition to use, false = to black, true is to screen
 	
-	public ContentPanel(int width, int height)
+	//these are different channel values for the transition fader to perform the LookupOp
+	private byte[] red;
+	private byte[] green;
+	private byte[] blue;
+	private byte[] alpha;
+	private byte[][] data;	//all the channels together for the lookup table
+	
+	public ContentPanel()
 	{
-		setSize(width, height);
-		setVisible(true);
-		
 		engine = Engine.getInstance();
 		clearColor = DEFAULT_CLEAR_COLOR;
 		try {
@@ -49,6 +76,13 @@ public class ContentPanel extends JPanel{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		red   = new byte[256];
+		green = new byte[256];
+		blue  = new byte[256];
+		alpha = new byte[256];
+		
+		font = SFont.loadFont("default", 24.0f);
 	}
 		
 	/**
@@ -68,10 +102,51 @@ public class ContentPanel extends JPanel{
 	 */
 	public void evokeTransition(boolean t)
 	{
+		//create the buffer if it hasn't been yet
+		if (tBuffer == null)
+		{
+			tBuffer = new BufferedImage(INTERNAL_RES_W, INTERNAL_RES_H, BufferedImage.TYPE_4BYTE_ABGR);
+			if (tBuffer == null) {
+				System.out.println("transitioning buffer is null");
+				return;
+			}
+			else
+				tGraphics = tBuffer.getGraphics();
+		}
+		//get a copy of what was last drawn to screen
+		tBuffer.getGraphics().drawImage(dbImage, 0, 0, null);
+		
+		//true if fading into scene
 		if (t)
+		{
+			//transition starts at 0 so the whole buffer will be black
 			transition = 0;
+			for (int i = 0; i < 256; i++)
+			{
+				//all colors are set to black for drawing
+				red[i] = green[i] = blue[i] = (byte)0;
+				//whole thing is opaque
+				alpha[i] = (byte) 255;
+			}
+		}
+		//false if fading out
 		else
+		{
+			//transition starts at 255 so it transitions from white to black on the image for the fader
 			transition = TRANSITIONLIMIT;
+			for (int i = 0; i < 256; i++)
+			{
+				//make everything black, but it hasn't started to fade yet
+				red[i] = green[i] = blue[i] = (byte)0;
+				alpha[i] = (byte)0;	
+			}
+		}
+		
+		//make the data array
+		//buffered image is loaded as argb, so it needs to be in that order
+		data = new byte[][]{alpha, red, green, blue};
+		
+		//set the transition direction variable
 		transIn = t;
 	}
 	
@@ -80,10 +155,14 @@ public class ContentPanel extends JPanel{
 	 */
 	public boolean isTransitioning()
 	{
+		boolean t;
+		//if transitioning it, it goes from 0 to 255
 		if (transIn)
-			return transition < TRANSITIONLIMIT;
+			t = transition < TRANSITIONLIMIT;
+		//transitioning out is 255 to 0
 		else
-			return transition > 0;
+			t = transition > 0;
+		return t;
 	}
 	
 	/**
@@ -91,11 +170,10 @@ public class ContentPanel extends JPanel{
 	 */
 	public void render()
 	{
-
 		//create the buffer if it hasn't been yet
 		if (dbImage == null)
 		{
-			dbImage = createImage(INTERNAL_RES_W, INTERNAL_RES_H);
+			dbImage = new BufferedImage(INTERNAL_RES_W, INTERNAL_RES_H, BufferedImage.TYPE_4BYTE_ABGR);
 			if (dbImage == null) {
 				System.out.println("dbImage is null");
 				return;
@@ -111,64 +189,58 @@ public class ContentPanel extends JPanel{
 				setClearColor(engine.getCurrentScene().getDisplay().getClearColor());
 		
 		//clear the buffer
-		//but not when transitioing
-		if (!isTransitioning())
-		{
-			dbg.setColor(clearColor);
-			dbg.fillRect(0, 0, INTERNAL_RES_W, INTERNAL_RES_H);
-		}
+		dbg.setColor(clearColor);
+		dbg.fillRect(0, 0, INTERNAL_RES_W, INTERNAL_RES_H);
 		
 		//draw the current scene
 		if (engine.getCurrentScene() != null)
 			engine.getCurrentScene().render(dbg);	
-		
-		//draw the transition
-		if (transition <= TRANSITIONLIMIT && transFader != null)
-		{
-			transBuffer = new BufferedImage(stepTransition(), transFader.getRaster(), false, null);
-
-			dbg.drawImage(transBuffer, 0, 0, INTERNAL_RES_W, INTERNAL_RES_H, null);
-			transBuffer = null;
-		}
 	}
 	
 	/**
 	 * Updates the transition
 	 * @return
 	 */
-	private IndexColorModel stepTransition()
+	private void stepTransition()
 	{
-		//bytes are 256 because of 32-bit png
-		byte[] r = new byte[TRANSITIONLIMIT];
-		byte[] g = new byte[TRANSITIONLIMIT];
-		byte[] b = new byte[TRANSITIONLIMIT];
-		byte[] a = new byte[TRANSITIONLIMIT];
-		Arrays.fill(r, (byte)clearColor.getRed());
-		Arrays.fill(g, (byte)clearColor.getGreen());
-		Arrays.fill(b, (byte)clearColor.getBlue());
-		Arrays.fill(a, (byte)(TRANSITIONLIMIT-1));
+		//when transitioning in, we're turning black to clear
+		if (transIn)
+			alpha[transition] = 0;
+		//when transitioning out we're having the black crawl in
+		else
+			alpha[transition] = (byte)255;
 		
-		for (int i = 0; i < transition; i++)
-		{
-			r[i] = (byte) 255;
-			g[i] = (byte) 255;
-			b[i] = (byte) 255;
-			a[i] = 0;
-		}
+		//create the lookupop and apply it to the fader to create the transBuffer
+		// this transbuffer is then drawn on top of the saved screen
+		LookupTable lookupTable = new ByteLookupTable(0, data);
+		LookupOp op = new LookupOp(lookupTable, null);
+		transBuffer = op.filter(transFader, transBuffer);
 		
+		//advance the transition point to continue the fader
 		transition += ((transIn)?1:-1)*TRANSITIONRATE;
-		return new IndexColorModel(4, TRANSITIONLIMIT, r, g, b, a);
 	}
 	
 	/**
 	 * Paints the buffer to the panel
 	 */
-	public void paint()
+	public void paint(Graphics g)
 	{
-		Graphics graphics = getGraphics();
-		render();
-		
-		if (dbImage != null)
-			graphics.drawImage(dbImage, 0, 0, getWidth(), getHeight(), null);
+		if (isTransitioning())
+		{
+			System.out.println(transition);
+			if (tBuffer != null)
+			{
+				g.drawImage(tBuffer, 0, 0, getWidth(), getHeight(), null);
+				g.drawImage(transBuffer, 0, 0, getWidth(), getHeight(), null);
+			}
+			stepTransition();
+		}
+		else
+		{
+			tBuffer = null;
+			render();
+			if (dbImage != null)
+				g.drawImage(dbImage, 0, 0, getWidth(), getHeight(), null);
+		}
 	}	
 }
